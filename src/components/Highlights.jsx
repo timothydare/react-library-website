45791446886c257bf0876d83d004667a60b36ab3
Timothy <tedare115@gmail.com>
2022-08-23 23:49:49 -0700

    first commit

---
+++ src/components/Highlights.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Highlights = () => {
+  return (
+    <section id='highlights'>
+        <div className="container">
+            <div className="row">
+                <h2 className="section__title">Why choose <span className="purple">Library</span></h2>
+                <div className="highlight__wrapper">
+                    <div className="highlight">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Highlights
