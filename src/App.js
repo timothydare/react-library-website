c14e9483692a40db745e08f1c2ab582a5eac8299
Timothy <tedare115@gmail.com>
2022-08-25 18:42:07 -0700

    home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Nav from "./components/Nav";
+import Landing from "./components/Landing";
+import Highlights from "./components/Highlights"
+import Features from "./components/Features"
+import Discounted from "./components/Discounted";
+import Explore from "./components/Explore";
+import Footer from "./components/Footer";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Landing />
+      <Highlights />
+      <Features />
+      <Discounted />
+      <Explore />
+      <Footer />
     </div>
   );
 }
