1dcd8096a08259781336502d2cbf52037cf41272
Timothy <tedare115@gmail.com>
2022-08-27 00:31:16 -0700

    book info page

--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -1,10 +1,13 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import { Link, NavLink, useParams } from 'react-router-dom'
+import Book from '../components/ui/Book'
 import Price from '../components/ui/Price'
 import Rating from '../components/ui/Rating'
 
 const BookInfo = ({ books }) => {
+  const { id } = useParams();
+  const book = books.find(book => +book.id === +id)
   return (
     <div id='books__body'>
         <main id="books__main">
@@ -20,13 +23,13 @@ const BookInfo = ({ books }) => {
                     </div>
                     <div className="book__selected">
                       <figure className="book__selected--figure">
-                        <img src="https://coderslegacy.com/wp-content/uploads/2022/07/1_P7pTGa-PMfCq1VWuNJioig.png" alt="" className="book__selected-img" />
+                        <img src={book.url} />
                       </figure>
                       <div className="book__selected--description">
-                        <h2 className="book__selected--title">Crack the coding interview</h2>
-                        <Rating rating="4.5" />
+                        <h2 className="book__selected--title">{book.title}</h2>
+                        <Rating rating={book.rating} />
                         <div className="book__selected--price">
-                          <Price originalPrice={50} salePrice={20} />
+                          <Price originalPrice={book.originalPrice} salePrice={book.salePrice} />
                         </div>
                         <div className="book__summary">
                           <div className="book__summary-title">Summary</div>
@@ -42,7 +45,14 @@ const BookInfo = ({ books }) => {
               <div className="row">
                 <div className="book__selected--top">
                   <h2 className="book__selected--title--top">Recommended Books</h2>
-
+                </div>
+                <div className="books">
+                  {
+                    books
+                    .filter((book) => book.rating === 5 && +book.id !== +id)
+                    .slice(0, 4)
+                    .map((book) => <Book book={book} key={book.id} />)
+                  }
                 </div>
               </div>
             </div>
